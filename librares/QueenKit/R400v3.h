d0097cd9c5e27ac795ac469246ccc088c8300856
MeGum <81900588+Anon242@users.noreply.github.com>
2025-03-18 16:00:06 +0000

    bug fix

---
+++ librares/QueenKit/R400v3.h
@@ -0,0 +1,44 @@
+#pragma once
+// Настройки конфигурации разведенной платы, обязательны перед вызовом библиотеки
+// Авто переключение приема предачи
+#define SWITCH true 
+#include "QueenKit.h"
+
+class Board : public QueenKit {
+public:
+  Board(uint8_t id) {
+    // Назначем id
+  
+    QueenKit::id = id;
+  }
+
+  // Функция задаем порты, включаем Serial для шины
+  void init(void (*function)() = [](){}) {
+    
+    QueenKit::init();
+    // Назначаем attached function
+    attachFunction(function);
+ 
+    // Настраиваем порты
+    setPorts();
+  }
+
+  uint8_t in() // Функция чтения байта на входе платы
+  {
+    return ~(((PINC & 0x08) >> 3) | ((PINC & 0x04) >> 1) |
+             ((PINC & 0x02) << 1));
+  }
+
+  void out(uint8_t x) // Функция вывода байта на выход платы
+  {
+    PORTB = (PORTB & 0xF9) | ((x & 0x03) << 1);
+    PORTD = (PORTD & 0x9F) | ((x & 0x0C) << 3);
+  }
+
+private:
+  void setPorts() {
+    DDRC = 0b00000000;
+    DDRD |= 0b01100000;
+    DDRB |= 0b00000110;
+  }
+};
---
+++ librares/QueenKit/examples/QueenUnisense3v1/QueenUnisense3v1.ino
@@ -0,0 +1,32 @@
+#include <QueenUnisense3v1.h>
+
+#define DEVICE_ID 1
+Board queen(DEVICE_ID);
+
+//===================================================================================
+//      Filename  : QueenUnisense3v1
+//      Author    : MeGum
+//      Created   : 20.01.2025 16:24:54
+//      Version   : 1.0
+//      Notes     :
+//                :
+//===================================================================================
+
+void setup() { queen.init(onMessage); }
+
+void loop() { queen.loop(); }
+
+void onMessage() {
+  
+  // Получаем буфер ADC
+  uint16_t *adcBuffer = queen.inADC();
+  for (uint8_t i = 0; i < 16; i++) {
+    uint8_t bitIndex = 16 + 10 * i;
+    // Назначаем PWM из шины
+    queen.pwmOuts(queen.getBits(bitIndex, 10), i + 1);
+    // Отправляем данные ADC в шину
+    queen.setBits(bitIndex, 10, adcBuffer[i]);
+  }
+  // Отправляем цифровые пины в шину
+  queen.setBits(0, 16, queen.in());
+}
